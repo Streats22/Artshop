e041fd4be1d6c2a88984fd4623fcab311c92c42d
MaxBouricius <MaxBouricius@hotmail.com>
2021-12-08 19:35:22 +0100

    update

--- index.php
+++ index.php
@@ -6,6 +6,19 @@
      <title>Document</title>
 </head>
 <body>
-     <h1>Welkom op de homepage</h1>
-</body>
-</html>
+<?php
+if ( have_posts() ) :
+     while ( have_posts() ) : the_post(); ?>
+
+          <h2><?php the_title()?></h2>
+          <?php the_content() ?>
+          <a href="<?php the_permalink()?>">Lees meer</a>
+          <hr>
+     <?php endwhile;
+
+
+else :
+     echo '<p>There are no posts!</p>';
+
+endif;
+?>
