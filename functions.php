7cf3706c7031c2d4e00079a86f5a3055c6fc06bd
Robin <24046@ma-web.nl>
2021-12-15 14:47:37 +0100

    update repo

---
+++ functions.php
@@ -0,0 +1,21 @@
+<?php
+
+function stedelijkProject_load_script_styles(){
+    wp_enqueue_style('main-css', get_template_directory_uri() . '/css/style.css');
+   /* wp_enqueue_style('custom-css', get_template_directory_uri() . '/css/art-theme.css');*/
+
+}
+add_action( 'wp_enqueue_scripts', 'stedelijkProject_load_script_styles' );
+
+function stedelijkProject_init_theme() {
+    register_nav_menus(
+        array(
+            'header-menu' => __( 'Header Menu' ),
+            'extra-menu' => __( 'Extra Menu' ),
+            'footer-menu'=> __('Footer menu')
+        )
+    );
+}
+add_action( 'init', 'stedelijkProject_init_theme' );
+
+?>
--- index.php
+++ index.php
@@ -1,11 +1,31 @@
-<html lang="en">
-<head>
-     <meta charset="UTF-8">
-     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>Document</title>
-</head>
-<body>
-     <h1>Welkom op de homepage</h1>
-</body>
-</html>
+<?php
+get_header();
+?>
+
+
+<?php
+
+
+
+if ( have_posts() ) :
+    while ( have_posts() ) : the_post(); ?>
+        <h2><?php the_title() ?></h2>
+            <?php the_content() ?>
+            <hr>
+            <a href=" <?php the_permalink() ?>">Lees meer </a>
+
+    <?php endwhile;
+
+else :
+    echo '<p>There are no posts!</p>';
+
+endif;
+
+
+
+?>
+
+
+<?php
+get_footer();
+?>
